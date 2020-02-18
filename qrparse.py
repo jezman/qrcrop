f7db25bb7589a07c97f5abff055a27d4bc6f1c42
Sergey <sgmaslov@yandex.ru>
2020-02-18 14:19:11 +0300

    Get qr codes from labels.

---
+++ qrparse.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pdftotext
+
+UPLOAD_PATH = './originals'
+SPLITTED_PATH = './splitted'
+# symbols = {
+#     '&': '&amp',
+#     '\"': '&quot',
+#     '\'': '&apos',
+#     '>': '&gt',
+#     '<': '&lt',
+# }
+
+to_remove = ['МОДЕЛЬ', 'РАЗМЕР', '--', '']
+
+
+def get_codes(pdf):
+    codes = list()
+
+    for page in pdf:
+        page = page.split('\n')
+        page = [code.strip() for code in page if code not in to_remove]
+        six_codes = ([''.join(page[i:i + 2]) for i in range(0, len(page), 2)])
+        [codes.append(code) for code in six_codes]
+
+    return codes
+
+
+def create_csv(pdf_file):
+    with open(UPLOAD_PATH + '/' + pdf_file, "rb") as f:
+        pdf = pdftotext.PDF(f)
+        codes = get_codes(pdf)
+        np.savetxt(
+            '{}/Splitted_{}.csv'.format(SPLITTED_PATH, pdf_file.split('.')[0]),
+            codes, fmt='"%s"', delimiter=",")
